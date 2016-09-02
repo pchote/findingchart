e0275f83658a490ac8ffdc71198465b199106264
Paul Chote <p.chote@warwick.ac.uk>
2016-09-02 12:47:41 +0100

    Add README and LICENCE files.

---
+++ README.md
@@ -0,0 +1,4 @@
+A Flask + Bootstrap + jQuery web tool for generating finding charts for astronomical targets with proper motions.
+Source images are pulled from the STScl Digitized Sky Survey archive, and annotated with the estimated target position at the requested observing epoch.
+
+Annotated charts are displayed in the browser, and can be downloaded as a zip file.
--- findingchart.py
+++ findingchart.py
@@ -1,16 +1,16 @@
 #
-# make-pm-findingchart is free software: you can redistribute it and/or modify
+# findingchart is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# make-pm-findingchart is distributed in the hope that it will be useful,
+# findingchart is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with make-pm-findingchart.  If not, see <http://www.gnu.org/licenses/>.
+# along with findingchart.  If not, see <http://www.gnu.org/licenses/>.
 
 # pylint: disable=invalid-name
 
